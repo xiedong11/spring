947bddf8072a58254795ed04ba265764494e4ada
xiedong <xieguodong@chinanews.com.cn>
2020-04-13 16:43:33 +0800

    mybatis整理

---
+++ spring-mybatis/src/main/java/com/zhuandian/mapper/StudentMapper.java
@@ -0,0 +1,9 @@
+package com.zhuandian.mapper;
+
+import com.zhuandian.pojo.Student;
+
+public interface StudentMapper {
+    public Student findStudentById(int id);
+
+//    https://www.cnblogs.com/wugongzi/p/11726285.html
+}
